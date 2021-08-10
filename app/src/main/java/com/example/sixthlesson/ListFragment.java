1382b79ea977b75f20dba2ecaf8184041f1d9278
SergeYudkin <87810884+SergeYudkin@users.noreply.github.com>
2021-08-11 01:10:12 +0300

    Создал новый проект

---
+++ app/src/main/java/com/example/sixthlesson/ListFragment.java
@@ -0,0 +1,10 @@
+package com.example.sixthlesson;
+
+import androidx.fragment.app.Fragment;
+
+public class ListFragment extends Fragment {
+
+    public static ListFragment newInstance() {
+
+    }
+}
