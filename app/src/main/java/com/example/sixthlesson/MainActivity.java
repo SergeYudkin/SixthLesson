b232742a235c06cea1ed2ec47a6eee17d348089d
SergeYudkin <87810884+SergeYudkin@users.noreply.github.com>
2021-08-12 22:06:12 +0300

    Сщздал фрагменты и макеты фрагментов

--- app/src/main/java/com/example/sixthlesson/MainActivity.java
+++ app/src/main/java/com/example/sixthlesson/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sixthlesson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListFragment listFragment = new ListFragment();
+        ListFragment listFragment =ListFragment.newInstance();
+        
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.listFrag,ListFragment.newInstance())
+                .commit();
+
+        if(getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contentFrag,TasksFragment.newInstance())
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/example/sixthlesson/TasksFragment.java
+++ app/src/main/java/com/example/sixthlesson/TasksFragment.java
@@ -9,19 +9,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class ListFragment extends Fragment {
+public class TasksFragment extends Fragment {
 
-    public static ListFragment newInstance() {
-        return  new ListFragment();
-
+    public static TasksFragment newInstance(){
+        return new TasksFragment();
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-
-        View view = inflater.inflate(R.layout.fragment_list,container,false);
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tasks_fragment,container,false);
         return view;
     }
 }
