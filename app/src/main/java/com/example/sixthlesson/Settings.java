934d9cdc78f39a8f9d0ee9ec854118be1e62b923
SergeYudkin <87810884+SergeYudkin@users.noreply.github.com>
2021-08-14 23:00:40 +0300

    Начал ДЗ 7

---
+++ app/src/main/java/com/example/sixthlesson/Settings.java
@@ -0,0 +1,23 @@
+package com.example.sixthlesson;
+
+public class Settings {
+
+    public static final String SHARED_PREFERENCE_NAME = "SHARED_PREFERENCE_NAME";
+
+    public static final String IS_BACK_STACK_USED = "IS_BACK_STACK_USED";
+    public static final String IS_ADD_FRAGMENT_USED = "IS_ADD_FRAGMENT_USED";
+    public static final String IS_BACK_REMOVE_FRAGMENT = "IS_BACK_REMOVE_FRAGMENT";
+    public static final String IS_DELETE_FRAGMENT_BEFORE_ADD = "IS_DELETE_FRAGMENT_BEFORE_ADD";
+
+    public static boolean sharedPreference;
+
+    public static boolean isBackStack;
+    public static boolean isAddFragment;
+    public static boolean isBackRemove;
+    public static boolean isDeleteFragment;
+
+
+
+
+
+}
